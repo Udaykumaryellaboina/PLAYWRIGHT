db0c61e686892388e1a98ca9a21521d99b9dd5c2
Udaykumaryellaboina <yellaboinaudaykumar@gmail.com>
2025-07-22 18:29:53 +0530

    22072025

--- support/hooks.ts
+++ support/hooks.ts
@@ -1,9 +1,10 @@
-import { BeforeAll, Before, AfterAll, After } from '@cucumber/cucumber';
+import { BeforeAll, Before, AfterAll, After, Status, setDefaultTimeout } from '@cucumber/cucumber';
 import { Browser, chromium } from '@playwright/test';
 import { CustomWorld } from './custom-world';
-import { setDefaultTimeout } from '@cucumber/cucumber';
-setDefaultTimeout(60 * 1000); // 60 seconds
+import fs from 'fs-extra';
+import path from 'path';
 
+setDefaultTimeout(60 * 1000); // 60 seconds
 
 let browser: Browser;
 
@@ -16,7 +17,20 @@ Before(async function (this: CustomWorld) {
   this.page = await this.context.newPage();
 });
 
-After(async function (this: CustomWorld) {
+After(async function (this: CustomWorld, { pickle, result }) {
+  if (result?.status === Status.FAILED) {
+    const screenshotDir = path.join('reports', 'screenshots');
+    await fs.ensureDir(screenshotDir);
+
+    const sanitizedTitle = pickle.name.replace(/[^a-zA-Z0-9]/g, '_');
+    const screenshotPath = path.join(screenshotDir, `${sanitizedTitle}.png`);
+
+    await this.page.screenshot({ path: screenshotPath, type: 'png' });
+
+    const imageBuffer = await fs.readFile(screenshotPath);
+    await this.attach(imageBuffer, 'image/png');
+  }
+
   await this.page.close();
   await this.context.close();
 });
